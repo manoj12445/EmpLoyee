3438584f4196dfc9e72427be74cd56729fe437f3
Manoj68-saun <samantnarayan69@gmail.com>
2024-10-08 13:04:40 +0530

    ini

---
+++ Grid/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import {EmployeeTable} from "./Components/EmployeeTable/EmployeeTable"
+
+
+function App() {
+
+
+  return (
+    <>
+     <EmployeeTable/>
+    </>
+  )
+}
+
+export default App
